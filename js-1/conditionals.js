de2a04a83b7de4e6d905e2f8ac78d8bd3994ab66
Paola <paolabricenov@outlook.com>
2023-03-10 20:45:53 -0500

    Mejoras presentación

--- js-1/conditionals.js
+++ js-1/conditionals.js
@@ -1,14 +1,19 @@
-function calculaColor (num){
-}let color = "";
-if (num === 1){
-    color = "black";
-}else if (num === 2) {
-    color = "white";
-}else if(num === 3){
-    color = "red"
-}else{
-    color = "green"
+function calculaColor(num) {
+    let color = "";
+    if (num === 1) {
+        color = "black";
+    } else if (num === 2) {
+        color = "white";
+    } else if (num === 3) {
+        color = "red";
+    } else {
+        color = "green";
+    }
+
+    return "The color is " + color;
 }
 
-return "The color is " + color;
-
+console.log(calculaColor(1));
+console.log(calculaColor(2));
+console.log(calculaColor(3));
+console.log(calculaColor(8));
