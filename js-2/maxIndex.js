e308fd76039855977738fc0ef9af99f2eccdde93
Paola <paolabricenov@outlook.com>
2023-03-14 16:21:57 -0500

    Correcciones

--- js-2/maxIndex.js
+++ js-2/maxIndex.js
@@ -1,17 +1,17 @@
-const maxIndex=function(array){
-    if(array.lenght===0)return -1;
+const maxIndex = function (array) {
+    if (array.length === 0) return -1;
 
-    let max_index=0;
+    let max_index = 0;
     let max = array[0];
-    for (let i=1; i<array.lenght; i++){
-        if(max<array[i]){
-            max=array[i];
-            max_index=i;
+    for (let i = 1; i < array.length; i++) {
+        if (max < array[i]) {
+            max = array[i];
+            max_index = i;
         }
     }
     return max_index;
 }
 
-console.log(maxIndex([1, 3, 2])) // 1
-console.log(maxIndex([10, 9, 8, 7, 6, 5, 4])) // 0
-console.log(maxIndex([])) // -1
+console.log(maxIndex([1, 3, 2])); // 1
+console.log(maxIndex([10, 9, 8, 7, 6, 5, 4])); // 0
+console.log(maxIndex([])); // -1
